581c646618329b40b67e01668e9d61c90a6efb9c
catchamonkey <chris@sedlmayr.co.uk>
2011-10-19 16:02:34 +0000

    Adds a customisable fadeInTime, fadeOutDelay and fadeOutTime Fixes an issue with having auto fade out that stopped the click handler doing it's thing due to the delay being queued. Tidies the formatting of the unminified version to help debugging Adds minified version including latest changes

--- getnoticed.js
+++ getnoticed.js
@@ -1,9 +1,9 @@
 (function($) {
-    $.getnoticed = {'element':'p'}
+    $.getnoticed = {'element':'p', 'type':'info', 'fadeInTime':500, 'fadeOutDelay':10000, 'fadeOutTime':600}
     
     $.getnoticed.add = function(params) {
-        if(!params.title || !params.text || !params.type) {
-            throw 'You need to fill out the minumum 3 params: "title", "text" and type'; 
+        if(!params.title || !params.text) {
+            throw 'You need to fill out the minumum 2 params: "title" and "text"';
         }
         // override the defaults with supplied params
         for (var obj in params)
@@ -15,14 +15,23 @@
         response += ' class="'+$.getnoticed.type+' notification"><em>';
         response += $.getnoticed.title+':</em> ';
         response += $.getnoticed.text;
-        response += ' <a href="#">x</a></'+$.getnoticed.element+'>';
-        $('#notifications').append(response);
+        response += ' <a class="getnoticedClose" href="#">x</a></'+$.getnoticed.element+'>';
+        $(response)
+            .hide()
+            .appendTo('#notifications')
+            .fadeIn($.getnoticed.fadeInTime)
+            .delay($.getnoticed.fadeOutDelay)
+            .fadeOut($.getnoticed.fadeOutTime, function() {
+                $(this).remove();
+            });
     }
-
-    $(".notification a").live('click', function(event) {
-        $(this).parents($.getnoticed.element+":eq(0)").fadeOut(300, function() {
-            $(this).remove();
-        });
+    $("a.getnoticedClose").live('click', function(event) {
         event.preventDefault();
+        $(this)
+            .parents($.getnoticed.element+":eq(0)")
+            .stop()
+            .fadeOut($.getnoticed.fadeOutTime, function() {
+                $(this).remove();
+            });
     });
 })( jQuery );
