1163a904fc5245c7b30db2581e3d05e0df2b3bd3
Chris Sedlmayr <chris@sedlmayr.co.uk>
2014-04-17 20:21:27 +0100

    * Changes default element to li from p * Tweaks structure of HTML for notification * Change from old live event, to binding on body * Adds shortcut method to add error notifications

--- getnoticed.js
+++ getnoticed.js
@@ -1,40 +1,47 @@
 (function($) {
     $.getnoticed = {
-        'element':'p', 'title':'Info', 'type':'info',
-        'addDuration':500, 'removeDelay':6000, 'removeDuration':600
-    };
-    
+        'element':'li', 'title':'Info', 'type':'info',
+        'addDuration':500, 'removeDelay':2000, 'removeDuration':600
+    }
+
     $.getnoticed.add = function(params) {
         if(!params.text) {
-            throw 'You need to provide the "text" param';
+            throw 'You need to provide the "text" param'
         }
         // override the defaults with supplied params
-        $.extend($.getnoticed, params);
-        var response = '';
-        response += '<'+$.getnoticed.element;
-        response += ' class="'+$.getnoticed.type+' notification">';
-        response += '<div class="getnoticedTitle"><span>'+$.getnoticed.title+'</span></div> ';
-        response += '<span class="getnoticedText">'+$.getnoticed.text+'</span>';
-        response += ' <a class="getnoticedClose" href="#">x</a></'+$.getnoticed.element+'>';
+        $.extend($.getnoticed, params)
+        var response = ''
+        response += '<'+$.getnoticed.element
+        response += ' class="'+$.getnoticed.type+' notification">'
+        response += '<strong>'+$.getnoticed.title+'</strong> '
+        response += '<span class="getnoticedText">'+$.getnoticed.text+'</span>'
+        response += ' <a class="getnoticedClose" href="#">x</a></'+$.getnoticed.element+'>'
         $(response)
             .hide()
             .appendTo('#notifications')
             .fadeIn($.getnoticed.addDuration)
             .delay($.getnoticed.removeDelay)
-            .hide("slide", {direction: "up"}, $.getnoticed.removeDuration, function() {
-                $(this).before('<div style="height:'+$(this).height()+'px;" class="placeholder">');
-                $(this).after('</div>');
-                $(this).remove();
-                $('.placeholder').slideUp();
-            });
+            .fadeOut($.getnoticed.removeDuration, function() {
+                $(this).remove()
+            })
     };
-    $(".notification").live('click', function(event) {
-        event.preventDefault();
+
+    $.getnoticed.error = function(error) {
+        // set new defaults based on the error context
+        $.getnoticed.type = 'error';
+        $.getnoticed.title = 'Oops!';
+
+        if (typeof error == 'string') {
+            return $.getnoticed.add({text:error})
+        };
+
+        return $.getnoticed.add(error)
+    };
+
+    $("body").on('click', '.notification', function(event) {
+        event.preventDefault()
         $(this).stop().hide("slide", {direction: "up"}, $.getnoticed.removeDuration, function() {
-            $(this).before('<div style="height:'+$(this).height()+'px;" class="placeholder">');
-            $(this).after('</div>');
-            $(this).remove();
-            $('.placeholder').slideUp();
-        });
-    });
+            $(this).remove()
+        })
+    })
 })( jQuery );
